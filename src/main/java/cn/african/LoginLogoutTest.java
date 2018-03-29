b2b7776a0718f418d0aa1af8a230da73e919a17a
zhoumingjun <africancu@126.com>
2018-03-29 18:33:38 +0800

    Initial commit

---
+++ src/main/java/cn/african/LoginLogoutTest.java
@@ -0,0 +1,36 @@
+package cn.african;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginLogoutTest {
+    @Test
+    public void testHelloWorld(){
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        //3、将securityManager设置到当前运行环境中
+        SecurityUtils.setSecurityManager(securityManager);
+        //4、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        //5、这里的帐号和密码 将来是由用户输入进去
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        try {
+            //6、登录，即身份验证
+            subject.login(token);
+            System.out.println("登陆成功");
+        } catch (AuthenticationException e) {
+            //7、身份验证失败
+            System.out.println("登录失败");
+            e.printStackTrace();
+        }
+        //8、退出
+        subject.logout();
+    }
+}
